dc3fe7fedecbd4273065d8422eabe1fd9699ee23
3245katumatakaede <skewed0619@gmail.com>
2016-01-30 13:32:31 +0900

    カウントダウン　更新

--- GGJ_beta/Assets/KATSUMATA/TimeScript.cs
+++ GGJ_beta/Assets/KATSUMATA/TimeScript.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class TimeScript : MonoBehaviour {
-
+    public GameObject manager;
+    public Text text;
+    //カウント用
     public float StartTime;
-   
+    //配列
+    public string[] TimerBuf = new string[4];
 
 	void Start () {
-        //初期化
+        //初期化：カウント
         StartTime = 4;
         
 
@@ -17,12 +21,21 @@ public class TimeScript : MonoBehaviour {
 	void Update () {
         //一秒ずつ減らすよ
         StartTime -= Time.deltaTime;
+        //マイナスは表示されない
         if (StartTime < 0) StartTime = 0;
+        //テキスト表示
+        GetComponent<Text>().text = TimerBuf[((int)StartTime)].ToString();
 
-        if(StartTime == 4){
-
+        if(StartTime == 0){
+            //プレイヤーが動けるようにする
+            manager.GetComponent<GameManager>().SetMove(true);
         }
-
 	}
-    
+
+
+        
+
+        
+
+     
 }
