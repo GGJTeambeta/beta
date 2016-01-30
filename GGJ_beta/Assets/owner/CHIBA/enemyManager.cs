bc35d42be5201745b8e6a61941a8bb793b95e08a
inusaku <bigwave_whale@yahoo.co.jp>
2016-01-30 18:00:59 +0900

    奇跡を信じて

--- GGJ_beta/Assets/owner/CHIBA/enemyManager.cs
+++ GGJ_beta/Assets/owner/CHIBA/enemyManager.cs
@@ -6,9 +6,14 @@ public class enemyManager : MonoBehaviour {
 	public GameObject pos1;
 	public GameObject pos2;
 	public float speed;
+	private float sh;
+	private bool isAnim;
+//	public Transform target;
 	// Use this for initialization
 	void Start () {
+		isAnim = false;
 		isPlayer = false;
+		sh = Screen.height;
 	}
 	
 	// Update is called once per frame
@@ -20,13 +25,26 @@ public class enemyManager : MonoBehaviour {
 			GameObject.Find ("Enemy").GetComponent<Animator> ().SetBool ("isStart", false);
 		}
 
-		if(playerOut.outP1){
-			GameObject.Find ("Enemy").GetComponent<Animator> ().Stop();
-			// ターゲット方向のベクトルを求める
-			Vector3 vec = pos1.transform.position - transform.position;
-			// ターゲットの方向を向く
-			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(vec.x, 0, vec.z)), 10);
-			transform.Translate(Vector3.forward * speed); // 正面方向に移動
+		if (playerOut.outP1) {
+			GameObject.Find ("Enemy").GetComponent<Animator> ().Stop ();
+			LookAt2D (pos1);
+			Move (pos1);
+			isAnim = true;
+		} else {
+			if(isAnim == true){
+				GameObject.Find ("Enemy").GetComponent<Animator> ().Play ("betaGirl", 0, 0.0f);
+				isAnim = false;
+			}
+		}
+		if (playerOut.outP2) {
+			GameObject.Find ("Enemy").GetComponent<Animator> ().Stop ();
+			LookAt2D (pos2);
+			Move (pos2);
+		} else {
+			if(isAnim == true){
+				GameObject.Find ("Enemy").GetComponent<Animator> ().Play ("betaGirl", 0, 0.0f);
+				isAnim = false;
+			}
 		}
 	}
 
@@ -39,5 +57,26 @@ public class enemyManager : MonoBehaviour {
 			GameObject.Find ("gameManager").GetComponent<GameManager>().SetGameFinish(col.gameObject.name);
 			Destroy (col.gameObject);
 		}
+	}
+	void LookAt2D(GameObject target)
+	{
+		// 指定オブジェクトと回転さすオブジェクトの位置の差分(ベクトル)
+		Vector3 pos = target.transform.position - transform.position;
+		// ベクトルのX,Yを使い回転角を求める
+		float angle = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg;
+		Quaternion rotation = new Quaternion();
+		// 回転角は右方向が0度なので-90しています
+		var screenPos = GameObject.Find("Main Camera").GetComponent<Camera>().WorldToScreenPoint(this.transform.position);
+//		if (screenPos.y > sh / 2) {
+			rotation.eulerAngles = new Vector3 (0, 0, angle);
+//		} else {
+		//	rotation.eulerAngles = new Vector3 (0, 180, angle + 90);		
+//		}
+		// 回転
+		transform.rotation = rotation;
+	}
+
+	void Move(GameObject target){
+		this.gameObject.GetComponent<Rigidbody>().velocity = (target.transform.position - this.transform.position).normalized * speed;
 	}
 }
